bf8b39be3facd03a00209987a9f233fc31089e9e
Nicholas Marshman <marshmann@duq.edu>
2018-03-19 07:51:58 -0400

    Emoji Prediction using Naive Bayes
    Using a small portion of Semeval's 2017 tweet corpus, predict an emoji that might be associated with a given tweet.
    
    Currently correct about 26% of the time, pretty nice actually!

---
+++ Data.java
@@ -0,0 +1,22 @@
+public class Data implements Comparable<Data>
+{
+  String word;
+  int total;
+  int[] arr;
+  
+  public Data(String word, int total, int[] arr){
+    this.word = word;
+    this.total = total;
+    this.arr = arr;
+  }    
+  public int compareTo(Data w){
+    if (this.total > w.total)
+      return 1;
+    else if (this.total < w.total)
+      return -1;
+    return 0;
+  }
+  public String toString() {
+	  return word + " " + total + " ";
+  }
+}
